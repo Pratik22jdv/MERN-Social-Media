13d7a78c7022d2091a26c5eed43f5af233b0f460
Pratik22jdv <prjadhav2209@gmail.com>
2022-05-12 20:22:37 +0530

    Like-Dislike-Get Post API

--- social-media-rest-api/routes/post.js
+++ social-media-rest-api/routes/post.js
@@ -53,4 +53,32 @@ router.delete("/:id", async (req, res)=>{
     }
 });
 
+//Like-Dislike Post
+router.put("/:id/like", async (req, res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId))
+        {
+            await post.updateOne( { $push: {likes:req.body.userId} } );
+            res.status(200).json("Post has been liked");
+        }
+        else 
+        {
+            await post.updateOne( { $pull: {likes:req.body.userId} } );
+            res.status(200).json("Post has been disliked");
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//Get Post
+router.get("/:id", async (req, res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
