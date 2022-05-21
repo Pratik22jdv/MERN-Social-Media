18e6c4878f172f01b78478c40d60ddca64b83a1c
Pratik22jdv <prjadhav2209@gmail.com>
2022-05-21 17:15:09 +0530

    Body components

--- mern-social-media/src/pages/home/Home.js
+++ mern-social-media/src/pages/home/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import Topbar from '../../components/Topbar';
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Rightbar from '../../components/rightbar/Rightbar';
+import Feed from '../../components/feed/Feed';
+import "./home.css";
 
 export default function Home() {
   return <div>
-  <Topbar />
+    <Topbar />
+    <div className="homeContainer">
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </div>
   </div>;
 }
