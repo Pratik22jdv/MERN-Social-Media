bcc7828c74c8962452ec4d1ddf30b6c7bf332eaa
Pratik22jdv <prjadhav2209@gmail.com>
2022-05-22 19:58:26 +0530

    sidebar - structure & style

--- mern-social-media/src/components/sidebar/Sidebar.jsx
+++ mern-social-media/src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,81 @@
 import React from 'react';
 import "./sidebar.css";
+import RssFeedIcon from '@mui/icons-material/RssFeed';
+import ChatIcon from '@mui/icons-material/Chat';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+//import Bookmark from '@mui/icons-material/Bookmark';
 
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            Sidebar
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <RssFeedIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <ChatIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Chats</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <OndemandVideoIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Videos</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <BookmarkIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Bookmarks</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <RssFeedIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <ChatIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Chats</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <OndemandVideoIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Videos</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <BookmarkIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Bookmarks</span>
+                    </li>
+                </ul>
+                <button className="sidebarButton">Show More</button>
+                <hr className="sidebarHr" />
+                <ul className="sidebarFriendList">
+                   <li>
+                       <img className="sidebarFriendImg" src = "/assets/person/1.jpg" alt=""/>
+                       <span className="sidebarFriendName">Pratik Jadhav</span>
+                   </li>
+                   <li>
+                       <img className="sidebarFriendImg" src = "/assets/person/1.jpg" alt=""/>
+                       <span className="sidebarFriendName">Pratik Jadhav</span>
+                   </li>
+                   <li>
+                       <img className="sidebarFriendImg" src = "/assets/person/1.jpg" alt=""/>
+                       <span className="sidebarFriendName">Pratik Jadhav</span>
+                   </li>
+                   <li>
+                       <img className="sidebarFriendImg" src = "/assets/person/1.jpg" alt=""/>
+                       <span className="sidebarFriendName">Pratik Jadhav</span>
+                   </li>
+                   <li>
+                       <img className="sidebarFriendImg" src = "/assets/person/1.jpg" alt=""/>
+                       <span className="sidebarFriendName">Pratik Jadhav</span>
+                   </li>
+                </ul>
+            </div>
         </div>
     );
 }
