57753227d37f41bbd0b374d657ac45a9ebd93b62
Pratik22jdv <prjadhav2209@gmail.com>
2022-05-28 15:39:27 +0530

    rightbar- structure & style

--- mern-social-media/src/components/rightbar/Rightbar.jsx
+++ mern-social-media/src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,93 @@ import "./rightbar.css";
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            Rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="giftIcon" src="/assets/gift.png" alt="" />
+                    <span className="birthdayText"><b> Virat</b> and <b>3 other friends</b> have their birthdays</span>
+                </div>
+
+                <img className="rightbarAd" src="/assets/ad.png" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img
+                                className="rightbarProfileImg"
+                                src="/assets/person/1.jpg"
+                                alt="" />
+                            <span className="rightbarOnline"></span>    
+                        </div>
+                        <span className="rightbarUsername">Pratik Jadhav</span>
+                    </li>
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img
+                                className="rightbarProfileImg"
+                                src="/assets/person/1.jpg"
+                                alt="" />
+                            <span className="rightbarOnline"></span>    
+                        </div>
+                        <span className="rightbarUsername">Pratik Jadhav</span>
+                    </li>
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img
+                                className="rightbarProfileImg"
+                                src="/assets/person/1.jpg"
+                                alt="" />
+                            <span className="rightbarOnline"></span>    
+                        </div>
+                        <span className="rightbarUsername">Pratik Jadhav</span>
+                    </li>
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img
+                                className="rightbarProfileImg"
+                                src="/assets/person/1.jpg"
+                                alt="" />
+                            <span className="rightbarOnline"></span>    
+                        </div>
+                        <span className="rightbarUsername">Pratik Jadhav</span>
+                    </li>
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img
+                                className="rightbarProfileImg"
+                                src="/assets/person/1.jpg"
+                                alt="" />
+                            <span className="rightbarOnline"></span>    
+                        </div>
+                        <span className="rightbarUsername">Pratik Jadhav</span>
+                    </li>
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img
+                                className="rightbarProfileImg"
+                                src="/assets/person/1.jpg"
+                                alt="" />
+                            <span className="rightbarOnline"></span>    
+                        </div>
+                        <span className="rightbarUsername">Pratik Jadhav</span>
+                    </li>
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img
+                                className="rightbarProfileImg"
+                                src="/assets/person/1.jpg"
+                                alt="" />
+                            <span className="rightbarOnline"></span>    
+                        </div>
+                        <span className="rightbarUsername">Pratik Jadhav</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
