ef107364f742e850cad67aef6f14a5a46de15927
Pratik22jdv <prjadhav2209@gmail.com>
2022-05-29 17:04:39 +0530

    Dynamic Data - Posts in Feed

--- mern-social-media/src/components/Post/Post.jsx
+++ mern-social-media/src/components/Post/Post.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import "./post.css";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {Users} from "../../dummyData"
 
-export default function Posts() {
+export default function Posts({post}) {
     return (
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img className="postProfileImage" src="/assets/person/1.jpg" alt="" />
-                        <span className="postUsername">Pratik Jadhav</span>
-                        <span className="postDate">5 min ago</span>
+                        <img className="postProfileImage" src={Users.filter((u) => u.id === post?.userId)[0].profilePicture} alt="" />
+                        <span className="postUsername">{Users.filter((u) => u.id === post?.userId)[0].username}</span>
+                        <span className="postDate">{post.date}</span>
                     </div>
 
                     <div className="postTopRight"> <MoreVertIcon /> </div>
@@ -18,19 +19,19 @@ export default function Posts() {
 
 
                 <div className="postCenter">
-                    <span className="postText">Its My First Post:)</span>
-                    <img className="postImage" src="/assets/person/1.jpg" alt="" />
+                    <span className="postText">{post?.desc}</span>
+                    <img className="postImage" src={post.photo} alt="" />
                 </div>
 
                 <div className="postBottom">
                     <div className="BottomLeft">
                         <img className="likeIcon" src="/assets/like.png" alt="" />
                         <img className="likeIcon" src="/assets/heart.png" alt="" />
-                        <span className="postLikeCounter">100 peple like it</span>
+                        <span className="postLikeCounter">{post.like} peple like it</span>
                     </div>
 
                     <div className="postBotttomRight">
-                        <div className="postCommentText">39 comments</div>
+                        <div className="postCommentText">{post.comment} comments</div>
                     </div>
                 </div>
             </div>
--- mern-social-media/src/components/feed/Feed.jsx
+++ mern-social-media/src/components/feed/Feed.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Share from '../share/Share';
-import Posts from '../Post/Post';
+import Post from '../Post/Post';
 import "./feed.css";
+import {Posts} from "../../dummyData"
 
 export default function Feed() {
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
-                <Posts />
-                <Posts />
-                <Posts />
+                {Posts.map((p)=> (
+                    <Post key = {p.id} post = {p} />
+                )
+                )}
             </div>
         </div>
     );
