abe2beb9e7774811a3380e9bcfada9ab769bebfe
Pratik22jdv <prjadhav2209@gmail.com>
2022-06-03 16:46:17 +0530

    Login page

--- mern-social-media/src/App.js
+++ mern-social-media/src/App.js
@@ -1,9 +1,10 @@
 //import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
+//import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Login />
     </div>
   );
 }
