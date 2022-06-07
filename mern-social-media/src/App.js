6305e132e9dffde3205cf20dfca26c6c27cf02a2
Pratik22jdv <prjadhav2209@gmail.com>
2022-06-07 16:47:49 +0530

    Modifications in right bar

--- mern-social-media/src/App.js
+++ mern-social-media/src/App.js
@@ -1,12 +1,13 @@
-//import Home from "./pages/home/Home";
-//import Profile from "./pages/profile/Profile";
-//import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home/Home";
+// import Profile from "./pages/profile/Profile";
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+    <Home />
     </div>
   );
 }
